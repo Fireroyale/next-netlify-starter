e8428c625d111e86024a3cd8dfc41defe5816c52
Fireroyale <64291728+Fireroyale@users.noreply.github.com>
2022-01-24 18:24:12 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,14 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
+    <Head>
+    <main> {
+  <background: #050930;/>
+  <border-radius: 5px;>
+  <padding: 0.75rem;>
+  <font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;>
+}
+
       <Head>
         <title>MASKED</title>
         <link rel="icon" href="/favicon.ico" />
@@ -20,7 +28,6 @@ export default function Home() {
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
     Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
-
 
 <main> {
   <background: #050930;/>
