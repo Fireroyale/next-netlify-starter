de0a8934c5aa8ae6abc53061520737a31c28dc13
Fireroyale <64291728+Fireroyale@users.noreply.github.com>
2022-01-24 18:08:51 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
 
 
 <main> {
-  <background: #050930;/>
+  <background=: #050930;/>
   <border-radius: 5px;>
   <padding: 0.75rem;>
   <font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;>
