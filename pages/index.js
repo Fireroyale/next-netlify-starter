7e63937284af0b95ea19fb185f01bf29992f3eee
Fireroyale <64291728+Fireroyale@users.noreply.github.com>
2022-01-24 17:23:33 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Account Masked!" />
+        <Header title="AccountMask" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
