99f21079af72dab6180bd8a5c60f509a7dea0ddb
Fireroyale <64291728+Fireroyale@users.noreply.github.com>
2022-01-24 18:54:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,46 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-    <Head>
-  
-    }
       <Head>
-        <title>MASKED</title>
+        <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Yo"/>
+        <Header title="AccountMask" />
         <p className="description">
-        Main page
+          Accounts from anygames can we sell here
 <body> {
   padding: 0;
   margin: 0;
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
     Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
-<main>{
-<body style="background-color:#050930"/>
-          }
+
+* {
+  box-sizing: border-box;
+}
+
+main {
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+<code> {
+  background: #050930;
+  border-radius: 5px;
+  padding: 0.75rem;
+  font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+}
+
+.container {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
