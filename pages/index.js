57ff3075677baef18438161f70255dd208627fc2
Fireroyale <64291728+Fireroyale@users.noreply.github.com>
2022-01-24 19:06:54 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
@@ -12,23 +11,50 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="AccountMask" />
-        <p className="description"/>
-    Accounts from anygames
-<body> {
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell,
-    Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-}
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
 
+      <Footer />
 
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
+        }
+      `}</style>
 
-
-
-<code> {
-  <background: #050930;>
-  <border-radius: 5px;>
-  <padding: 0.75rem;>
-  <font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
 }
