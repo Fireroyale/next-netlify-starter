b3f9e786f48189e3fa0a01eb2fb9b631044c9996
Fireroyale <64291728+Fireroyale@users.noreply.github.com>
2022-01-24 19:00:42 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
@@ -11,9 +12,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Play Time" />
+        <Header title="AccountMask" />
         <p className="description">
-         Main page 
+          Accounts from anygames can we sell here
 <body> {
   padding: 0;
   margin: 0;
@@ -21,30 +22,13 @@ export default function Home() {
     Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
 
-* {
-  box-sizing: border-box;
-}
 
-main {
-  padding: 5rem 0;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+
+
 
 <code> {
-  background: #050930;
-  border-radius: 5px;
-  padding: 0.75rem;
-  font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;
-}
-
-.container {
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  <background: #050930;>
+  <border-radius: 5px;>
+  <padding: 0.75rem;>
+  <font-family: Menlo, Monaco, Lucida Console, Courier New, monospace;>
 }
